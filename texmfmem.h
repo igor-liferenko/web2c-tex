b3056499d97c94961eed65f288be81e553b734bb
Igor Liferenko <igor.liferenko@gmail.com>
2018-10-17 08:43:26 +0700

    revert changes to texmfmem.h until you finish tex.ch

--- texmfmem.h
+++ texmfmem.h
@@ -43,10 +43,27 @@
    twohalves.u:    B1    B0  JNK1  JNK2
    fourquarters:   B3    B2    B1    B0
    
+   In Aleph, quarterwords are two octets, so the picture becomes simpler:
+   
+   BigEndian:
+   twohalves.v:  RH_MM RH_ML RH_LM RH_LL LH_MM LH_ML LH_LM LH_LL
+   twohalves.u:  ---------JUNK---------- ----B0----- ----B1-----
+   fourquarters: ----B0----- ----B1----- ----B2----- ----B3-----
+   twoints:      ---------CINT0--------- ---------CINT1---------
+   
+   LittleEndian:
+   twohalves.v:  LH_LL LH_LM LH_ML LH_MM RH_LL RH_LM RH_ML RH_MM
+   twohalves.u:  ----B1----- ----B0-----
+   fourquarters: ----B3----- ----B2----- ----B1----- ----B0-----
+   twoints:      ---------CINT1--------- ---------CINT0---------
+   
    This file can't be part of texmf.h, because texmf.h gets included by
    {tex,mf,mp}d.h before the `halfword' etc. types are defined.  So we
    include it from the change file instead.
 */
+
+/* Aleph is sufficiently different to separate the definition. */
+#if !defined(Aleph) && !defined(epTeX) && !defined(eupTeX) && !defined(upTeX)
 
 typedef union
 {
@@ -95,6 +112,9 @@ typedef union
 #else
   twohalves hhfield;
 #endif
+#ifdef XeTeX
+  voidpointer ptr;
+#endif
 #ifdef WORDS_BIGENDIAN
   integer cint;
   fourquarters qqqq;
@@ -109,9 +129,11 @@ typedef union
 
   struct
   {
+#ifndef XeTeX
 #if defined (TeX) && !defined (SMALLTeX) || defined (MF) && !defined (SMALLMF) || defined (MP) && !defined (SMALLMP)
     halfword junk;
 #endif /* big {TeX,MF,MP} */
+#endif
     fourquarters QQQQ;
   } v;
 #endif /* not WORDS_BIGENDIAN */
@@ -128,6 +150,9 @@ typedef union
 #else /* not WORDS_BIGENDIAN */
   struct
   {
+#ifdef XeTeX
+    halfword junk; /* quarterword is really 16 bits in XeTeX, so integer does not fill the union */
+#endif
     integer CINT;
   } u;
 
@@ -155,3 +180,81 @@ typedef union
 #ifndef WORDS_BIGENDIAN
 #define qqqq v.QQQQ
 #endif
+
+#else /* Aleph || epTeX || eupTeX || upTeX */
+
+typedef union
+{
+  struct
+  {
+#ifdef WORDS_BIGENDIAN
+    halfword RH, LH;
+#else
+    halfword LH, RH;
+#endif
+  } v;
+
+  struct
+  { /* Make B0,B1 overlap the most significant bytes of LH.  */
+#ifdef WORDS_BIGENDIAN
+    halfword junk;
+    quarterword B0, B1;
+#else /* not WORDS_BIGENDIAN */
+  /* If 32-bit memory words, have to do something.  */
+#if defined (SMALLTeX) || defined (SMALLMF) || defined (SMALLMP)
+    fixme
+#else
+    quarterword B1, B0;
+#endif /* big memory words */
+#endif /* LittleEndian */
+  } u;
+} twohalves;
+
+typedef struct
+{
+  struct
+  {
+#ifdef WORDS_BIGENDIAN
+    quarterword B0, B1, B2, B3;
+#else
+    quarterword B3, B2, B1, B0;
+#endif
+  } u;
+} fourquarters;
+
+typedef struct
+{
+#ifdef WORDS_BIGENDIAN
+  integer CINT0, CINT1;
+#else
+  integer CINT1, CINT0;
+#endif
+} twoints;
+  
+typedef struct
+{
+  glueratio GLUE;
+} glues;
+
+typedef union
+{
+  twohalves hh;
+  fourquarters qqqq;
+  twoints ii;
+  glues gg;
+} memoryword;
+
+#define b0 u.B0
+#define b1 u.B1
+#define b2 u.B2
+#define b3 u.B3
+
+#define rh v.RH
+#define lhfield v.LH
+
+#define cint ii.CINT0
+#define cint1 ii.CINT1
+
+#define gr gg.GLUE
+
+#endif /* Aleph || epTeX || eupTeX || upTeX */
