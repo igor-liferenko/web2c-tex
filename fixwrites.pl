b9a8f8a86b20d9612d319042a620f864caf44ffa
Igor Liferenko <igor.liferenko@gmail.com>
2020-02-05 14:07:45 +0700

    add fixwrites.pl

---
+++ fixwrites.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -0777 -p
+s/write\(([^,]+),\s*([^"]+?)\)/putc($2, $1)/g;
+s/write\(([^,]+),\s*"(.+?)"/Fputs($1, "$2"/g;
+s/writeln\(([^,]+?)\)/putc('\\n', $1)/g;
+s/writeln\(([^,]+?),\s*([^,]+?)\)/fprintf($1, "%s\\n", $2)/g;
+s/writeln\(([^,]+?),\s*([^,]+?),\s*([^,]+?)\)/fprintf($1, "%s%s\\n", $2, $3)/g;
+s/writeln\(([^,]+?),\s*([^,]+?),\s*([^,]+?),\s*([^,]+?)\)/fprintf($1, "%s%s%ld\\n", $2, $3, (long) $4)/g;
+__END__
+
+This script does the following substitutions:
+
+write(x, y); -> putc(y, x);
+write(x, "y"); -> Fputs(x, "y");
+writeln(x); -> putc('\n', x);
+writeln(x, y); -> fprintf(x, "%s\n", y);
+writeln(x, y, z); -> fprintf(x, "%s%s\n", y, z);
+writeln(x, y, z, v); -> fprintf(x, "%s%s%ld\n", y, z, (long) v);
