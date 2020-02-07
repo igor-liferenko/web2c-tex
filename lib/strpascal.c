7418cfaf4fcf3fa566cbd4f9ae9fb5972bb395ae
Igor Liferenko <igor.liferenko@gmail.com>
2020-02-07 09:16:00 +0700

    style fix

--- lib/strpascal.c
+++ lib/strpascal.c
@@ -6,53 +6,39 @@
    start after the leading character Pascal puts in, and terminate it
    with a null.  */
 
-void
-make_c_string (p_string)
-    char **p_string;
+void make_c_string(char **p_string)
 {
   (*p_string)++;
-  null_terminate (*p_string);
+  null_terminate(*p_string);
 }
-
 
 /* Replace the first space we come to with a null.  */
 
-void
-null_terminate (s)
-    char *s;
+void null_terminate(char *s)
 {
-  while (*s != ' ')
-    s++;
-
+  while (*s != ' ') s++;
   *s = 0;
 }
 
-
 /* Change the C string C_STRING into a Pascal string; i.e., make it
    start one character before it does now (so C_STRING had better have
-   been a Pascal string originally), and terminate with a space.  */
+   been a Pascal string originally), and terminate with a space. */
 
-void
-make_pascal_string (c_string)
-    char **c_string;
+void make_pascal_string(char **c_string)
 {
-  space_terminate (*c_string);
+  space_terminate(*c_string);
   (*c_string)--;
 }
 
 
 /* Replace the first null we come to with a space.  */
 
-void
-space_terminate (s)
-    char *s;
+void space_terminate(char *s)
 {
-  for ( ; *s != 0; s++)
-    ;
-  
+  while (*s != 0) s++;
   *s = ' ';
 }
-
+
 /* Change the suffix of BASE (a Pascal string) to be SUFFIX (a
    C string), if BASE does not already have a suffix.  We have to change
    them to C strings to do the work, then convert them back to Pascal
