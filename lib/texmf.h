d841368f39dade66ca83bb854820336fd62343fe
Igor Liferenko <igor.liferenko@gmail.com>
2020-02-10 13:23:30 +0700

    move cpascal.h to texmf.h

--- lib/texmf.h
+++ lib/texmf.h
@@ -10,7 +10,86 @@ extern boolean wopenin();
 #define wopenout bopenout
 #define wclose(f) if (f) fclose(f)
 
-#include "cpascal.h"
+/* cpascal.h: implement various bits of standard Pascal that we use.
+   This is the top-level include file for all the web2c-generated C
+   programs except TeX and Metafont themselves, which use texmf.h.  */
+
+#include "config.h"
+
+/* Absolute value.  Without the casts to integer here, the Ultrix and
+   AIX compilers (at least) produce bad code (or maybe it's that I don't
+   understand all the casting rules in C) for tests on memory fields. 
+   Specifically, a test in diag_round (in Metafont) on a quarterword
+   comes out differently without the cast, thus causing the trap test to
+   fail.  (A path at line 86 is constructed slightly differently).  */
+#ifdef abs
+/* If the system had an abs #define already, get rid of it.  */
+#undef abs
+#endif
+#define	abs(x) ((integer)(x) >= 0 ? (integer)(x) : (integer)-(x))
+
+/* Other standard predefined routines.  */
+#define	chr(x)		(x)
+#define ord(x)		(x)
+#define	odd(x)		((x) % 2)
+#define	round(x)	zround ((double) (x))
+#define trunc(x)	((integer) (x))
+
+#define read(f, b)	((b) = getc (f))
+#define	readln(f)	{ register int c; \
+                          while ((c = getc (f)) != '\n' && c != EOF); }
+
+/* PatGen 2 uses this.  */
+#define	input2ints(a, b)  zinput2ints (&a, &b)
+
+/* We need this routine only if TeX is being debugged.  */
+#define	input3ints(a, b, c)  zinput3ints (&a, &b, &c)
+
+/* Pascal has no address-of operator, and we need pointers to integers
+   to set up the option table.  */
+#define addressofint(x)	(&(x))
+
+/* Not all C libraries have fabs, so we'll roll our own.  */
+#ifdef fabs
+#undef fabs
+#endif
+#define	fabs(x)		((x) >= 0.0 ? (x) : -(x))
+
+/* The fixwrites program outputs this.  */
+#define	Fputs(f, s)	(void) fputs (s, f)
+
+/* Same underscore problem.  */
+#define PATHMAX		PATH_MAX
+
+#define printreal(r, n, m)  fprintreal (stdout, r, n, m)
+#define	putbyte(x, f)	putc ((char) (x) & 255, f)
+
+#define	toint(x)	((integer) (x))
+
+/* For throwing away input from the file F.  */
+#define vgetc(f)	(void) getc (f)
+
+/* Write out elements START through END of BUF to the file F.  */
+#define writechunk(f, buf, start, end) \
+  (void) fwrite (&buf[start], sizeof (buf[start]), end - start + 1, f)
+
+/* Like fseek(3), but cast the arguments and ignore the return value.  */
+#define checkedfseek(f, n, w)  (void) fseek(f, (long) n, (int) w)
+
+/* For faking arrays.  */
+typedef unsigned char *pointertobyte;
+#define casttobytepointer(e) ((pointertobyte) e)
+
+/* For some initializations of constant strings.  */
+typedef char *ccharpointer;
+
+/* `real' is used for noncritical floating-point stuff.  */
+typedef double real;
+
+/* C doesn't distinguish between text files and other files.  */
+typedef FILE *text, *file_ptr, *alphafile;
+
+
 
 #define dump_file fmtfile
 #define dump_path TEXFORMATPATH
